ca18e49968c923e6e9363e0b8344e0dc4df2682e
Lahmar22 <lahmarzakariae22@gmail.com>
2026-09-15 21:34:53 +0100

    feat : created the connection a postgre

---
+++ connectionDB.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+import os
+from dotenv import load_dotenv
+
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
+db_name = os.getenv("DB_NAME")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+
+DATABASE_URL = f"postgresql+psycopg://{db_user}:{db_password}@{db_host}:{5432}/{db_name}"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(bind=engine)
+
+
+class Base(DeclarativeBase):
+    pass
